f3d6439994a201e0749eb96b01d601824e39da07
helongfei <643756132@qq.com>
2022-01-02 23:06:12 +0800

    Update storage.d.ts

--- release/storage.d.ts
+++ release/storage.d.ts
@@ -1,16 +1,15 @@
-import { clear, get, remove, save } from '../src/storage';
 declare const Storage: {
-    get: typeof get;
-    save: typeof save;
-    remove: typeof remove;
-    clear: typeof clear;
-    session: {
-        get: (key: string) => string | null;
-        save: (key: string, value: string) => void;
-        remove: (key: string) => void;
-        clear: () => void;
-    };
-    on: any;
-    off: any;
+  get: (key: string) => string | null;
+  save: (key: string, value: string, time: number) => void;
+  remove: (key: string) => void;
+  clear: () => void;
+  session: {
+    get: (key: string) => string | null;
+    save: (key: string, value: string) => void;
+    remove: (key: string) => void;
+    clear: () => void;
+  };
+  on: any;
+  off: any;
 };
 export default Storage;
